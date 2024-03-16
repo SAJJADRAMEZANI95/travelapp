7db8a7057c6bb75cabec1e39a5c7e28a9962b3ac
sajjad ramezani <sajjadramezani780@gmail.com>
2024-03-17 00:39:08 +0330

    WIP:edit Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className=' flexBetween max-container padding-container relative z-30 py-5'>
+        <Link href="/">
+           <Image src="/hilink-logo.svg" alt='logo' width={74} height={29}/> 
+        </Link>
+    </nav>
   )
 }
 
